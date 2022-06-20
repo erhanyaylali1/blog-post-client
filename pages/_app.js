7ac16fb3aa4a742da717c2b67fae8744cb6fdcc8
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2022-06-20 19:43:41 +0300

    app js is configured for reset password link

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Nprogress from 'nprogress';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import 'antd/dist/antd.css';
 import '../public/style.css';
 
@@ -8,5 +8,9 @@ Router.onRouteChangeComplete = () => Nprogress.done();
 Router.onRouteChangeError = () => Nprogress.done();
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  if (router.asPath.includes('#reset/')) {
+    router.replace(router.asPath.replace('#', ''));
+  }
   return <Component {...pageProps} />;
 }
