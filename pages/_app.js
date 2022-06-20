2f4f8d994f9ae976cfbe92b478cce7858705a29f
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2022-06-20 19:52:05 +0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Nprogress from 'nprogress';
-import Router, { useRouter } from 'next/router';
+import Router from 'next/router';
 import 'antd/dist/antd.css';
 import '../public/style.css';
 
@@ -8,9 +8,5 @@ Router.onRouteChangeComplete = () => Nprogress.done();
 Router.onRouteChangeError = () => Nprogress.done();
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  if (router.asPath.includes('#reset/')) {
-    router.replace(router.asPath.replace('#', ''));
-  }
   return <Component {...pageProps} />;
 }
