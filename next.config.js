881ad2a45c862855b7e61344795f6743017b4c54
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2022-06-21 21:21:40 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   publicRuntimeConfig: {
     APP_NAME: 'BLOG',
-    API_URL: 'http://localhost:8080',
-    // API_URL:
-    //   'http://blogpost-env-1.eba-prgmrppq.us-east-2.elasticbeanstalk.com',
+    // API_URL: 'http://localhost:8080',
+    API_URL:
+      'http://blogpost-env-1.eba-prgmrppq.us-east-2.elasticbeanstalk.com',
     PRODUCTION: false,
   },
 };
