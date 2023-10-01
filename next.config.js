6d197d2f73f31e36d48212b5eb6d91b7833bcbab
eranyaylali <erhanyaylali9@gmail.com>
2023-10-01 11:53:41 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  publicRuntimeConfig: {
-    APP_NAME: 'BLOG',
-    // API_URL: 'http://localhost:8080',
-    API_URL: 'https://blogpost-354108.ey.r.appspot.com',
-    PRODUCTION: false,
-  },
+    publicRuntimeConfig: {
+        APP_NAME: 'BLOG',
+        API_URL: 'http://localhost:8080',
+        PRODUCTION: false,
+    },
 };
